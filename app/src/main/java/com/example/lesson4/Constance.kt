e6b94595cfeb44385c0fc045679161a29b5d5e29
regaliv <regaliv@example.com>
2023-09-16 23:09:29 +0300

    Constance - это объект

---
+++ app/src/main/java/com/example/lesson4/Constance.kt
@@ -0,0 +1,11 @@
+package com.example.lesson4
+
+object Constance {
+    const val INGENER_SUELDO = 10000
+    const val DVORNIK_SUELDO = 1000
+    const val DIRECTOR_SUELDO = 100000
+
+    const val DIRECTOR="Egor"
+    const val INGINER ="Andrey"
+    const val DVORNIK ="Sergey"
+}
---
+++ app/src/main/java/com/example/lesson4/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.lesson4
+
+
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.view.View
+import com.example.lesson4.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bindingClass : ActivityMainBinding  // ActivityMainBinding это тип разметки
+
+    override fun onCreate(s: Bundle?){
+        super.onCreate(s)
+        bindingClass=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
+        bindingClass.btResult.setOnClickListener {
+            // Log.d("MyLog2", "Result =  $resultValue")
+            when(bindingClass.edValue.text.toString()){
+
+                Constance.DIRECTOR -> {
+                    bindingClass.tvResult.visibility=View.VISIBLE
+                    val tempText = "Получите ваши ${Constance.DIRECTOR_SUELDO}"
+                    bindingClass.tvResult.text = tempText
+                }
+                Constance.INGINER ->{
+                    bindingClass.tvResult.visibility=View.VISIBLE
+                    val tempText = "Получите ваши ${Constance.INGENER_SUELDO}"
+                    bindingClass.tvResult.text = tempText
+                }
+                Constance.DVORNIK -> {
+                    bindingClass.tvResult.visibility=View.VISIBLE
+                    val tempText = "Получите ваши ${Constance.DVORNIK_SUELDO}"
+                    bindingClass.tvResult.text = tempText
+                }
+                else ->{
+                    bindingClass.tvResult.visibility=View.VISIBLE
+                    bindingClass.tvResult.text = "Нет такого работника"
+                }
+
+            }
+        }
+    }
+
+
+
+}
