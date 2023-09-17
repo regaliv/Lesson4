838734bab1fe730440757ac938f3788da46cac90
regaliv <regaliv@example.com>
2023-09-18 00:11:31 +0300

    Самая Последняя версия выдачи зарплаты уже с паролем и с фото

--- app/src/main/java/com/example/lesson4/MainActivity.kt
+++ app/src/main/java/com/example/lesson4/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.lesson4
 
 
-
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -10,62 +8,70 @@ import android.view.View
 import com.example.lesson4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var bindingClass : ActivityMainBinding  // ActivityMainBinding это тип разметки
+    private lateinit var bindingClass: ActivityMainBinding  // ActivityMainBinding это тип разметки
 
-    override fun onCreate(s: Bundle?){
+    override fun onCreate(s: Bundle?) {
         super.onCreate(s)
-        bindingClass=ActivityMainBinding.inflate(layoutInflater)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
 
         bindingClass.btResult.setOnClickListener {
             // Log.d("MyLog2", "Result =  $resultValue")
-            bindingClass.imageView2.visibility=View.VISIBLE
-            when(bindingClass.edValue.text.toString()){
+            bindingClass.imageView2.visibility = View.VISIBLE
+            when (bindingClass.edValue.text.toString()) {
 
                 Constance.DIRECTOR -> {
-                    bindingClass.tvResult.visibility=View.VISIBLE
+                    bindingClass.tvResult.visibility = View.VISIBLE
                     val tempText = "Получите ваши ${Constance.DIRECTOR_SUELDO}"
-                    bindingClass.tvResult.text =
-                    if (bindingClass.editTextTextPassword2.text.toString()==Constance.DIRECTOR_PASSWORD){
-                        tempText
-                    }else{
-                        Constance.test2
+
+                    if (bindingClass.editTextTextPassword2.text.toString() == Constance.DIRECTOR_PASSWORD) {
+                        bindingClass.tvResult.text = tempText
+                        bindingClass.imageView2.setImageResource(R.drawable.face_coco)
+                    } else {
+                        bindingClass.tvResult.text = "Неверный пароль"
+                        bindingClass.imageView2.setImageResource(R.drawable.dula)
                     }
 
                 }
+
                 Constance.INGINER -> {
                     bindingClass.tvResult.visibility = View.VISIBLE
                     val tempText = "Получите ваши ${Constance.INGENER_SUELDO}"
-                    bindingClass.tvResult.text =
-                        if (bindingClass.editTextTextPassword2.text.toString() == Constance.INGINER_PASSWORD) {
-                            tempText
 
-                        } else {
-                            Constance.test2
-                        }
+                    if (bindingClass.editTextTextPassword2.text.toString() == Constance.INGINER_PASSWORD) {
+                        bindingClass.tvResult.text = tempText
+                        bindingClass.imageView2.setImageResource(R.drawable.face_coc)
+
+                    } else {
+                       bindingClass.tvResult.text = "Неверный пароль"
+                        bindingClass.imageView2.setImageResource(R.drawable.dula)
+
+                    }
                 }
+
                 Constance.DVORNIK -> {
                     bindingClass.tvResult.visibility = View.VISIBLE
                     val tempText = "Получите ваши ${Constance.DVORNIK_SUELDO}"
 
-                        if (bindingClass.editTextTextPassword2.text.toString() == Constance.DVORNIK_PASSWORD) {
-                            bindingClass.tvResult.text =  tempText
-                            bindingClass.imageView2.setImageResource(R.drawable.face_co)
+                    if (bindingClass.editTextTextPassword2.text.toString() == Constance.DVORNIK_PASSWORD) {
+                        bindingClass.tvResult.text = tempText
+                        bindingClass.imageView2.setImageResource(R.drawable.face_co)
 
-                        }
-                        else{
-                            bindingClass.tvResult.text = "Нет такого работника"
-                        }
+                    } else {
+                        bindingClass.tvResult.text = "Неверный пароль"
+                        bindingClass.imageView2.setImageResource(R.drawable.dula)
+                    }
                 }
-                else ->{
-                    bindingClass.tvResult.visibility=View.VISIBLE
+
+                else -> {
+                    bindingClass.tvResult.visibility = View.VISIBLE
                     bindingClass.tvResult.text = "Нет такого работника"
+                    bindingClass.imageView2.setImageResource(R.drawable.dula)
                 }
 
             }
         }
     }
-
 
 
 }
